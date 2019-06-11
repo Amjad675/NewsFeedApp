d6ef46eb0a576258aead5aeb6722cccf058ba37b
Amjad Hussain <amjad_675@yahoo.com>
2019-06-11 11:17:52 +0530

    PDF download

--- app/modules/home/scenes/PDFView/index.js
+++ app/modules/home/scenes/PDFView/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { StyleSheet, Dimensions, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  Button,
+  Platform
+} from "react-native";
 import Pdf from "react-native-pdf";
+import RNFetchBlob from "rn-fetch-blob";
 
 class PDFView extends Component {
   constructor(props) {
@@ -9,6 +17,51 @@ class PDFView extends Component {
       page: 1
     };
   }
+
+  onDownloadPDF() {
+    // let PictureDir =
+    //   Platform.OS === "ios" ? fs.dirs.DocumentDir : fs.dirs.PictureDir;
+    // RNFetchBlob.config({
+    //   fileCache: true,
+    //   addAndroidDownloads: {
+    //     useDownloadManager: true, // setting it to true will use the device's native download manager and will be shown in the notification bar.
+    //     notification: false,
+    //     description: "Downloading image."
+    //   },
+    //   path:
+    //     PictureDir + "/me_" + Math.floor(date.getTime() + date.getSeconds() / 2) // this is the path where your downloaded file will live in
+    // })
+    //   .fetch("GET", "http://samples.leanpub.com/thereactnativebook-sample.pdf")
+    //   .then(resp => {
+    //     // the path of downloaded file
+    //     // resp.path();
+    //   });
+
+    let reportDir =
+      Platform.OS === "ios"
+        ? RNFetchBlob.fs.dirs.DocumentDir
+        : RNFetchBlob.fs.dirs;
+
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        useDownloadManager: true, // setting it to true will use the device's native download manager and will be shown in the notification bar.
+        notification: true,
+        description: "PDF Report"
+      },
+      path: reportDir + "/report.pdf" // this is the path where your downloaded file will live in
+    };
+
+    RNFetchBlob.config(options)
+      .fetch("GET", "http://samples.leanpub.com/thereactnativebook-sample.pdf")
+      .then(res => {
+        console.log(RNFetchBlob.android.actionViewIntent(res.path(), "/"));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     const source = {
       uri: "http://samples.leanpub.com/thereactnativebook-sample.pdf",
@@ -29,6 +82,7 @@ class PDFView extends Component {
             title="Previous"
             onPress={() => this.setState({ page: this.state.page - 1 })}
           />
+          <Button title="Download" onPress={this.onDownloadPDF} />
           <Button
             title="Next"
             onPress={() => this.setState({ page: this.state.page + 1 })}
